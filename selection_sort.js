d01e41fc0a3dbd8061eae865d2028f20d8894290
Kannan <kannans02001@gmail.com>
2025-07-20 23:12:05 +0530

    Learned bubble sort

---
+++ selection_sort.js
@@ -0,0 +1,21 @@
+function selectionSort(arr) {
+     let n = arr.length;
+
+     for (let i = 0; i < n - 1; i++) {
+          let minIndex = i;
+          for (let j = i + 1; j < n; j++) {
+               if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+               }
+          }
+          if (minIndex !== i) {
+               let temp = arr[i];
+               arr[i] = arr[minIndex];
+               arr[minIndex] = temp;
+          }
+     }
+
+     return arr;
+}
+
+console.log(selectionSort([7, 2, 4, 1]))
